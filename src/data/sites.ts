2a4b0a647483234eca2a936384d53fda897aadde
Muhammad Faris Hadi Mulyo <kimfais1234@gmail.com>
2024-12-08 17:50:34 +0700

    feat: add SEO Layout

---
+++ src/data/sites.ts
@@ -0,0 +1,11 @@
+export default {
+	siteName: "Muhfarishalyo",
+	siteUrl: "https://muhfarishadimulyo.my.id/",
+	github: "https://github.com/Fariszz",
+	linkedin: "https://www.linkedin.com/in/muhammad-faris-hadi-mulyo/",
+	twitter: "https://x.com/fa_zzz19",
+	cv: "/assets/My Resume.pdf",
+	email: "muhfarishalyo@gmail.com",
+	description: "Muhammad Faris Hadi Mulyo personal website",
+	keywords: ["personal", "website", "blog", "article"],
+} as const;
